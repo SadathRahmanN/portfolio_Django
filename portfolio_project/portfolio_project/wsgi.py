9084e31e207e14d16346062be0664dd9d25a2b34
SADATHRAHMANN <sadathrahman.n@gmail.com>
2024-11-15 20:48:30 +0530

    2nd render

--- portfolio_project/portfolio_project/wsgi.py
+++ portfolio_project/portfolio_project/wsgi.py
@@ -1,16 +1,21 @@
-"""
-WSGI config for portfolio_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
 application = get_wsgi_application()
+
+# Ensure the app listens on the correct port for Render:
+if __name__ == "__main__":
+    from gunicorn.app.base import BaseApplication
+
+    class GunicornApplication(BaseApplication):
+        def __init__(self, *args, **kwargs):
+            self.cfg = gunicorn.config.Config()
+            self.cfg.set('bind', f'0.0.0.0:{os.environ.get("PORT", "8000")}')
+            super().__init__(*args, **kwargs)
+
+        def load(self):
+            return application
+
+    GunicornApplication().run()
