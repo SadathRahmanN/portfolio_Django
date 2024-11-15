680220671e6ab81447d00889a8b2ab97f04b9c16
SADATHRAHMANN <sadathrahman.n@gmail.com>
2024-11-15 21:14:36 +0530

    render 4

--- portfolio_project/portfolio_project/wsgi.py
+++ portfolio_project/portfolio_project/wsgi.py
@@ -5,17 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
 application = get_wsgi_application()
 
-# Ensure the app listens on the correct port for Render:
+# Only needed for production environments
 if __name__ == "__main__":
-    from gunicorn.app.base import BaseApplication
-
-    class GunicornApplication(BaseApplication):
-        def __init__(self, *args, **kwargs):
-            self.cfg = gunicorn.config.Config()
-            self.cfg.set('bind', f'0.0.0.0:{os.environ.get("PORT", "8000")}')
-            super().__init__(*args, **kwargs)
-
-        def load(self):
-            return application
-
-    GunicornApplication().run()
+    from waitress import serve
+    serve(application, host='127.0.0.1', port=8000)
